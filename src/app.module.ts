ca9fb2cb111501c8dc709838f8877f1b99508d0f
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2023-01-02 22:02:54 +0100

    cleaning the code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import DataSource from '../typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `./src/.env`,
+      envFilePath: `/.env`,
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       ...DataSource.options,
     }),
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
