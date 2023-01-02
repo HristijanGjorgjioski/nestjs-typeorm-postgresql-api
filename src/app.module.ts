921a4991dcfc9ff0bacefbad8a5dc486769bb02c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2023-01-02 18:32:11 +0100

    making better logic for dev and prod env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,26 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import DataSource from '../typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `./src/.env.${process.env.NODE_ENV}`,
+      envFilePath: `./src/.env`,
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      // database: 'nest_api_learning',
-      database: process.env.DATABASE_NAME, // TODO: fix this problem
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: ['dist/**/entities/*.entity.{ts,js}'],
-      migrations: ['dist/src/migrations/*.{ts,js}'],
-      migrationsTableName: 'typeorm_migrations',
+      ...DataSource.options,
     }),
     UsersModule,
   ],
